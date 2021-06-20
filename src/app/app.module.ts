41290ab3319cacb8b4a99d05787bc98cf610ebbf
enrico_gigante <enrico.gigante@gmail.com>
2021-06-20 17:22:46 +0200

    non funz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {MonthService, ScheduleModule, WeekService} from "@syncfusion/ej2-angular-schedule";
+import {CommonModule} from "@angular/common";
+import {RecurrenceEditorAllModule, ScheduleAllModule} from "@syncfusion/ej2-angular-schedule";
+import {DropDownListAllModule, MultiSelectAllModule} from "@syncfusion/ej2-angular-dropdowns";
+import {ButtonAllModule, CheckBoxAllModule, SwitchAllModule} from "@syncfusion/ej2-angular-buttons";
+import {
+  MaskedTextBoxModule,
+  NumericTextBoxAllModule,
+  TextBoxAllModule,
+  UploaderAllModule
+} from "@syncfusion/ej2-angular-inputs";
+import {DatePickerAllModule, DateTimePickerAllModule, TimePickerAllModule} from "@syncfusion/ej2-angular-calendars";
+import {ContextMenuAllModule, ToolbarAllModule, TreeViewModule} from "@syncfusion/ej2-angular-navigations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, ScheduleModule
-  ],
-  providers: [WeekService,
-    MonthService],
+  declarations: [AppComponent],
+  imports: [CommonModule, ScheduleAllModule, RecurrenceEditorAllModule, NumericTextBoxAllModule, TextBoxAllModule, DatePickerAllModule, TimePickerAllModule, DateTimePickerAllModule,
+    CheckBoxAllModule, ToolbarAllModule, DropDownListAllModule, ContextMenuAllModule,
+    MaskedTextBoxModule, UploaderAllModule, MultiSelectAllModule, TreeViewModule, ButtonAllModule, SwitchAllModule, BrowserModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
