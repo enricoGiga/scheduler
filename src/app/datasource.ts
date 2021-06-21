49cf0fd32e4a9d01e06fd769e7333231d8f9a71c
enrico_gigante <enrico.gigante@gmail.com>
2021-06-21 14:56:20 +0200

    non funz

--- src/app/datasource.ts
+++ src/app/datasource.ts
@@ -8,25 +8,28 @@ export let defaultData: Object[] = [
     Subject: 'Conference',
     StartTime: new Date(2021, 6, 14, 10, 0),
     EndTime: new Date(2021, 6, 14, 11, 0),
-    RecurrenceRule: "FREQ=DAILY;INTERVAL=2;COUNT=10"
+    RecurrenceRule: "FREQ=DAILY;INTERVAL=2;COUNT=10",
+    CategoryColor: '#357cd2'
   }, {
     Id: 2,
     Subject: 'Meeting - 1',
     StartTime: new Date(2021, 6, 15, 10, 0),
     EndTime: new Date(2021, 6, 16, 12, 30),
-    IsAllDay: false
+    IsAllDay: false,
+    CategoryColor: '#7fa900'
   }, {
     Id: 3,
     Subject: 'Paris',
     StartTime: new Date(2021, 6, 13, 12, 0),
     EndTime: new Date(2021, 6, 13, 12, 30),
     IsAllDay: false,
-    IsBlock: true
+    IsBlock: true,
   }, {
     Id: 4,
     Subject: 'Vacation',
     StartTime: new Date(2021, 6, 12, 10, 0),
     EndTime: new Date(2021, 6, 12, 10, 30),
-    IsAllDay: false
+    IsAllDay: false,
+    CategoryColor: '#ea7a57'
   }
 ];
